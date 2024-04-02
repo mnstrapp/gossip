dbb8b3dccce8770b7adeeb114f4f6e092ef92ee2
Michael Christenson II <m3talsmith@gmail.com>
2024-04-02 11:47:40 -0400

    Added log interceptor example

--- gossip/server.go
+++ gossip/server.go
@@ -1,10 +1,12 @@
 package gossip
 
 import (
+	"context"
 	"net"
 
 	"github.com/mnstrapp/gossip/log"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/peer"
 )
 
 type Server struct {
@@ -22,7 +24,26 @@ func (s *Server) Listen() error {
 		return err
 	}
 
-	server := grpc.NewServer()
+	opts := []grpc.ServerOption{
+		grpc.StreamInterceptor(func(srv any, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+			p, _ := peer.FromContext(ss.Context())
+			log.Infof("incoming stream request: %s->%s %s", p.Addr, p.LocalAddr, info.FullMethod)
+			if err := handler(srv, ss); err != nil {
+				log.Errorf("incoming stream error: %s->%s %s: %s", p.Addr, p.LocalAddr, info.FullMethod, err)
+			}
+			return err
+		}),
+		grpc.UnaryInterceptor(func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+			p, _ := peer.FromContext(ctx)
+			log.Infof("incoming unary request: %s->%s %s", p.Addr, p.LocalAddr, info.FullMethod)
+			if resp, err = handler(ctx, req); err != nil {
+				log.Errorf("incoming unary error: %s->%s %s: %s", p.Addr, p.LocalAddr, info.FullMethod, err)
+			}
+			return
+		}),
+	}
+
+	server := grpc.NewServer(opts...)
 	RegisterGossipApiServer(server, &service{})
 
 	return server.Serve(lis)
